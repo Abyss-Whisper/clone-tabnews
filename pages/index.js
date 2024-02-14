99216e7affd2c281f3f52733e78df3a307185172
Augusto Santana <61059576+Abyss-Whisper@users.noreply.github.com>
2024-02-14 02:40:00 +0000

    colocando mensagem fofa

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,10 @@
 function Home(){
-    /*var frase = [<h1>Muito obrigado por acessar meu primeiro site 😎</h1>,
-            <h4>Testando algumas coisas</h4>
+    var frase = [<h1>Oii, pai! Oii, mãe! Tudo bem?</h1>,
+            <h2>Aproveitem a viagem!! Amo vocês 🧡</h2>,
+            <h3>E vamos torcer juntos para o carro dar certo 🤞</h3>,
+            <p>Ass.: Augustinho</p>
         ] 
-    return frase*/
-
-    <h1>Muito obrigado por acessar meu primeiro site 😎</h1>,
-    <h4>Testando algumas coisas</h4>
-    
-return
+    return frase
     //return <h1>Muito obrigado por acessar meu primeiro site 😎. Estou testando algumas coisas</h1>
 
 }
