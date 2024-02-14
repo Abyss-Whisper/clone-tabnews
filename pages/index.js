97d7cd1848269a7f6feda35b1478baa96e83169f
Augusto Santana <61059576+Abyss-Whisper@users.noreply.github.com>
2024-02-14 02:27:05 +0000

    Removendo a variavel

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,14 @@
 function Home(){
-    return <h1>Muito obrigado por acessar meu primeiro site 😎</h1>
+    /*var frase = [<h1>Muito obrigado por acessar meu primeiro site 😎</h1>,
+            <h4>Testando algumas coisas</h4>
+        ] 
+    return frase*/
+
+    <h1>Muito obrigado por acessar meu primeiro site 😎</h1>,
+    <h4>Testando algumas coisas</h4>
+    
+return
+    //return <h1>Muito obrigado por acessar meu primeiro site 😎. Estou testando algumas coisas</h1>
+
 }
 export default Home;
